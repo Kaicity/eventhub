0ba8a744a2be70c22e0fba5c7227f2f4c1b81b5c
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-05-25 11:37:54 +0700

    feat: implement navigators and screens for Explore, Events, Add, Map, and Profile

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,12 +1,20 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {HomeScreen} from '../screens';
+import ExploreNavigator from './ExploreNavigator';
+import EventNavigator from './EventNavigator';
+import {AddNewScreen} from '../screens';
+import MapNavigator from './MapNavigator';
+import ProfileNavigator from './ProfileNavigator';
 
 const TabNavigator = () => {
   const Tab = createBottomTabNavigator();
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="Explore" component={ExploreNavigator} />
+      <Tab.Screen name="Events" component={EventNavigator} />
+      <Tab.Screen name="Add" component={AddNewScreen} />
+      <Tab.Screen name="Map" component={MapNavigator} />
+      <Tab.Screen name="Profile" component={ProfileNavigator} />
     </Tab.Navigator>
   );
 };
---
+++ src/screens/profiles/ProfilesScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ProfilesScreen = () => {
+  return (
+    <View>
+      <Text>profilesScreen</Text>
+    </View>
+  );
+};
+
+export default ProfilesScreen;
